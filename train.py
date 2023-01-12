d04e716af962421396973f1c03a45f299cf1258d
Priya Naphade <priyanaphade31@gmail.com>
2023-01-11 22:49:29 -0500

    arm extension analysis

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ h2 = np.array([44.81, 54.55, 43.27, 44.88])
 h3 = np.array([60.27, 39.53, 60.32, 58.23, 60.68, 109.46, 74.77, 51.64, 62.94, 67.57, 76.66])
 
 height_dists = [h0, h1, h2, h3]
-
+'''
 print("-------------------------------------")
 print("Height distribution quartiles")
 for dist in height_dists:
@@ -18,8 +18,7 @@ for dist in height_dists:
     print("Q3", np.quantile(dist, 0.75))
     print()
 print("-------------------------------------")
-
-
+'''
 #fig1 = sns.violinplot(data=height_dists)
 #fig1.set(xlabel="Height score", ylabel="Percent decrease in body height")
 
@@ -33,22 +32,31 @@ s2 = np.array([-48.44, -66.64, 7.96, -2.97])
 s3 = np.array([-52.7, -45.08, -58.20, 52.99, 4.97, -21.62])
 print("-------------------------------------")
 
-
 speed_dists = [s0, s1, s2, s3]
+'''
 print(len(speed_dists))
 for dist in speed_dists:
     print("Q1", np.quantile(dist, 0.25))
     print("Q3", np.quantile(dist, 0.75))
     print()
-
-#fig2 = sns.violinplot(data=speed_dists)
+'''
+#fig2 = sns.violinplot(data=speed_dists, cut=1)
 #fig2.set(xlabel="Acceleration score", ylabel="Percent change in velocity")
 #plt.show()
 
 
-# FLAG 13_003, 15_113, 21_000
-# Arm extension ratios
-a0 = [0.31, -1.0, 0.03]
-a1 = [0.29, -0.09, 0.16, -0.01, 0.13, 0.34]
-a2 = [0.61, 0.99, 0.38, 0.45, 0.76, 0.54]
-a3 = [0.28, 0.66, 0.67, 0.09, -0.22, 0.13, -0.08, -0.01, 0.7]
+
+# Arm angles [-pi/2, pi/2]
+a0 = np.array([-0.09, -0.63, 0.96, -0.67, -0.86])
+a1 = np.array([-1.42, 0.04, -1.43, -0.23, -0.61])
+a2 = np.array([-0.38, -0.35, -0.12, 0.48,])
+a3 = np.array([-0.29, -0.83, -0.73, 0.35, -0.80, 0.75, -0.08, -0.03, 0.33, 0.54])
+
+arm_dists = [a0, a1, a2, a3]
+for dist in arm_dists:
+    print("Q1", np.quantile(dist, 0.25))
+    print("Q3", np.quantile(dist, 0.75))
+    print()
+#fig3 = sns.violinplot(data=arm_dists, cut=0)
+#fig3.set(xlabel="Arm wrap score", ylabel="Angle between arms and torso (radians)")
+#plt.show()
